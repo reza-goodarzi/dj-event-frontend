56f4e03015509c9f164c891cd7ec50ddbef950ed
REZA <rezagoodarzi026@gmail.com>
2021-11-02 22:36:52 +0330

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is an app to find the latest and other musical Events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+function Event() {
+  return (
+    <div>
+      <h1>My Event</h1>
+    </div>
+  );
+}
+
+export default Event;
