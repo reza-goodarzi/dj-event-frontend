008bfc4ec37617ccef08a91dab00c6151963919e
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-03-22 03:29:32 +0430

    Displaying Event

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,11 +1,37 @@
 import Layout from "../../components/Layout";
+import { API_URL } from "../../config";
 
-function Event() {
+function Event({ evt }) {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{evt.name}</h1>
     </Layout>
   );
 }
 
 export default Event;
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const evt = await res.json();
+  return {
+    props: {
+      evt,
+    },
+    revalidate: 1,
+  };
+}
