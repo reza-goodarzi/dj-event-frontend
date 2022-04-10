26e3c4d28c650109689d4071cc831953e3cb96c2
Reza Goodarzi <Rezagoodarzi026@gmail.com>
2022-04-11 02:07:11 +0430

    Build dashboard UI

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,12 +1,44 @@
 import React from "react";
+import DashboardEvent from "../../components/DashboardEvent";
 import Layout from "../../components/Layout";
+import { API_URL } from "../../config";
+import { parseCookies } from "../../helper";
+import styles from "../../styles/Dashboard.module.css";
 
-function DashboardPage() {
+function DashboardPage({ events }) {
+  const deleteEvent = (id) => {
+    console.log(id);
+  };
+
   return (
     <Layout title="User Dashboard">
-      <h1>Dashboard</h1>
+      <div className={styles.dash}>
+        <h1>Dashboard</h1>
+        <h3>My Events</h3>
+
+        {events.map((evt) => (
+          <DashboardEvent key={evt.id} evt={evt} handleDelete={deleteEvent} />
+        ))}
+      </div>
     </Layout>
   );
 }
 
 export default DashboardPage;
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me?populate=image`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const { data: events } = await res.json();
+
+  return {
+    props: { events },
+  };
+}
